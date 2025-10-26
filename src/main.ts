1bfee92da0e9bbe01c6379a8407cbf7e9172c515
nxt964 <thanhnx964@gmail.com>
2025-10-26 16:22:42 +0700

    [Feature] Implement logging service with winston

--- src/main.ts
+++ src/main.ts
@@ -20,8 +20,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
-  console.log(`🚀 Server is running on http://localhost:3000`);
+  await app.listen(3001);
+  console.log(`🚀 Server is running on http://localhost:3001`);
   console.log(`📘 Swagger docs at http://localhost:3000/api`);
 }
 bootstrap();
